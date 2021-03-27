4c4ad4c8575163b9d74220419d71a38725d63989
Christian Bermeo <czaudiovisual@hotmail.com>
2021-03-26 23:49:40 -0400

    commit tree ac70b4dcc920c64b25d8856f92a2e61898f68b63

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+// 1
+const multiply = 62, num1 = multiply, num2 = multiply;
+multiply;
+
+// 2
+const random = 1;
+    Math.random() + random;
+
+// 3
+let num3 = 16
+let num4 = 4
+const mod = num3 / num4;
+ mod;
+
+// 4
+
+const max = Math.max(10,20);
